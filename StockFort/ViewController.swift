d2e8db8cbc8186a98a03a108b8833409625d0631
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-07-14 07:15:36 +0700

    feat: Setup initialization project

---
+++ StockFort/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockFort
+//
+//  Created by Rivaldo Fernandes on 14/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
